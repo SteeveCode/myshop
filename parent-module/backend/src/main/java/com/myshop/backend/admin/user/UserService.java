b7a9bd67384555f5571395ac7e210fdb60d93882
steevcode <stevecode@yahoo.com>
2023-03-17 17:04:36 +0100

    Section 6: Code User Management Module - Encode User Password

--- parent-module/backend/src/main/java/com/myshop/backend/admin/user/UserService.java
+++ parent-module/backend/src/main/java/com/myshop/backend/admin/user/UserService.java
@@ -4,6 +4,7 @@ package com.myshop.backend.admin.user;
 import com.myshop.common.entity.Role;
 import com.myshop.common.entity.User;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -17,6 +18,9 @@ public class UserService {
     @Autowired
     private RoleRepository roleRepo;
 
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
     public List<User> listAll() {
         return (List<User>) userRepo.findAll();
     }
@@ -26,6 +30,12 @@ public class UserService {
     }
 
     public void save(User user) {
+        encodePassword(user);
         userRepo.save(user);
+    }
+
+    private void encodePassword(User user) {
+        String encodedPassword = passwordEncoder.encode(user.getPassword());
+        user.setPassword(encodedPassword);
     }
 }
