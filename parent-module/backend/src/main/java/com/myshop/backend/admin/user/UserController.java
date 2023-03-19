fb1d046dae7451db9a541d8800102bca7ec3c965
steevcode <stevecode@yahoo.com>
2023-03-19 17:38:17 +0100

    Section 6: Code User Management Module - Code Update User Function

--- parent-module/backend/src/main/java/com/myshop/backend/admin/user/UserController.java
+++ parent-module/backend/src/main/java/com/myshop/backend/admin/user/UserController.java
@@ -1,11 +1,14 @@
 package com.myshop.backend.admin.user;
 
+import com.myshop.backend.admin.user.UserNotFoundException;
+import com.myshop.backend.admin.user.UserService;
 import com.myshop.common.entity.Role;
 import com.myshop.common.entity.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
@@ -34,6 +37,7 @@ public class UserController {
 
         model.addAttribute("user", user);
         model.addAttribute("listRoles", listRoles);
+        model.addAttribute("pageTitle", "Create New User");
 
         return "user_form";
     }
@@ -46,5 +50,24 @@ public class UserController {
         redirectAttributes.addFlashAttribute("message", "The user has been saved successfully.");
 
         return "redirect:/users";
+    }
+
+    @GetMapping("/users/edit/{id}")
+    public String editUser(@PathVariable(name = "id") Integer id,
+                           Model model,
+                           RedirectAttributes redirectAttributes) {
+        try {
+            User user = service.get(id);
+            List<Role> listRoles = service.listRoles();
+
+            model.addAttribute("user", user);
+            model.addAttribute("pageTitle", "Edit User (ID: " + id + ")");
+            model.addAttribute("listRoles", listRoles);
+
+            return "user_form";
+        } catch (UserNotFoundException ex) {
+            redirectAttributes.addFlashAttribute("message", ex.getMessage());
+            return "redirect:/users";
+        }
     }
 }
--- parent-module/backend/src/main/java/com/myshop/backend/admin/user/UserRestController.java
+++ parent-module/backend/src/main/java/com/myshop/backend/admin/user/UserRestController.java
@@ -11,7 +11,7 @@ public class UserRestController {
     private UserService service;
 
     @PostMapping("/users/check_email")
-    public String checkDuplicateEmail(@Param("email") String email) {
-        return service.isEmailUnique(email) ? "OK" : "Duplicated";
+    public String checkDuplicateEmail(@Param("email") String email, @Param("id") Integer id) {
+        return service.isEmailUnique(id, email) ? "OK" : "Duplicated";
     }
 }
