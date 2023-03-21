e93f99b9bb6b01e4e3fd2a533d1dfe466ad5b670
steevcode <stevecode@yahoo.com>
2023-03-21 12:38:00 +0100

    Section 6: Code User Management Module - Code Refactor #1

--- parent-module/backend/src/main/java/com/myshop/backend/admin/user/UserController.java
+++ parent-module/backend/src/main/java/com/myshop/backend/admin/user/UserController.java
@@ -110,7 +110,12 @@ public String listByPage(
 
         redirectAttributes.addFlashAttribute("message", "The user has been saved successfully.");
 
-        return "redirect:/users";
+        return getRedirectURLtoAffectedUser(user);
+    }
+
+    private String getRedirectURLtoAffectedUser(User user) {
+        String firstPartOfEmail = user.getEmail().split("@")[0];
+        return "redirect:/users/page/1?sortField=id&sortDir=asc&keyword=" + firstPartOfEmail;
     }
     @GetMapping("/users/edit/{id}")
     public String editUser(@PathVariable(name = "id") Integer id,
