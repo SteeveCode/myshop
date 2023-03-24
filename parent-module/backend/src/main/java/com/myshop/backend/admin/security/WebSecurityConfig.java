82f34c667be48c67ec76f6d4a2ac93c09c126e3c
steevcode <stevecode@yahoo.com>
2023-03-24 18:48:34 +0100

    Section 7: Code User Authentication - Code User Authentication

--- parent-module/backend/src/main/java/com/myshop/backend/admin/security/WebSecurityConfig.java
+++ parent-module/backend/src/main/java/com/myshop/backend/admin/security/WebSecurityConfig.java
@@ -2,16 +2,13 @@ package com.myshop.backend.admin.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -19,26 +16,21 @@ import org.springframework.security.web.SecurityFilterChain;
 public class WebSecurityConfig {
 
     @Bean
-    public PasswordEncoder PasswordEncoder() {
+    public UserDetailsService userDetailsService() {
+        return new MyshopUserDetailsService();
+    }
+    @Bean
+    public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
-
     @Bean
-    protected UserDetailsService userDetailsService() {
-        UserDetails user1 = User
-                .withUsername("john@gmail.com")
-//                .password("password")
-                .password("$2a$10$sWszOXuTlN0amQi8vXp4cerb.tJUQo.4FzLAnTCsSqChsYhlLdQWW")
-                .roles("USER")
-                .build();
-        UserDetails user2 = User
-                .withUsername("admin")
-                .password("$2a$10$PH0p2x2x8oi5bKx.80Bt7ubMAiHdZnqm9TC/Cpss9VoccyTYw1AoC")
-                .roles("ADMIN")
-                .build();
-
-        return new InMemoryUserDetailsManager(user1, user2);
+    public DaoAuthenticationProvider authenticationProvider(){
+        DaoAuthenticationProvider authprovider = new DaoAuthenticationProvider();
+        authprovider.setUserDetailsService(userDetailsService());
+        authprovider.setPasswordEncoder((passwordEncoder()));
+        return authprovider;
     }
+
     @Bean
     protected SecurityFilterChain  filterChain(HttpSecurity http) throws Exception {
         http.authorizeRequests()
