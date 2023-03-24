0c69116bf62142f7d7dc2e36dd99fa2662c3815b
steevcode <stevecode@yahoo.com>
2023-03-24 16:21:40 +0100

    Section 7: Code User Authentication - Code User Login Page

--- parent-module/backend/src/main/java/com/myshop/backend/admin/security/WebSecurityConfig.java
+++ parent-module/backend/src/main/java/com/myshop/backend/admin/security/WebSecurityConfig.java
@@ -3,24 +3,57 @@ package com.myshop.backend.admin.security;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig {
+
     @Bean
-    public PasswordEncoder passwordEncoder(){
+    public PasswordEncoder PasswordEncoder() {
         return new BCryptPasswordEncoder();
     }
 
     @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.authorizeHttpRequests().anyRequest().permitAll();
-//                requestMatchers("/", "/**").permitAll().and().formLogin();
+    protected UserDetailsService userDetailsService() {
+        UserDetails user1 = User
+                .withUsername("john@gmail.com")
+//                .password("password")
+                .password("$2a$10$sWszOXuTlN0amQi8vXp4cerb.tJUQo.4FzLAnTCsSqChsYhlLdQWW")
+                .roles("USER")
+                .build();
+        UserDetails user2 = User
+                .withUsername("admin")
+                .password("$2a$10$PH0p2x2x8oi5bKx.80Bt7ubMAiHdZnqm9TC/Cpss9VoccyTYw1AoC")
+                .roles("ADMIN")
+                .build();
 
-        return http.build();
+        return new InMemoryUserDetailsManager(user1, user2);
     }
+    @Bean
+    protected SecurityFilterChain  filterChain(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .anyRequest().authenticated()
+                .and()
+                .formLogin()
+                .loginPage("/login")
+                .usernameParameter("email")
+                .permitAll();
+               return http.build();
+    }
+    @Bean
+    public WebSecurityCustomizer webSecurityCustomizer() {
+        return (web) -> web.ignoring().requestMatchers("/images/**", "/js/**", "/webjars/**");
+    }
+
+
 }
