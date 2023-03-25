68341eba6cd86619d4cd1d94388161df57ec57b8
steevcode <stevecode@yahoo.com>
2023-03-25 21:18:57 +0100

    Section 7: Code User Authentication -  Show User's Full Name and Logout Link

--- parent-module/backend/src/main/java/com/myshop/backend/admin/security/MyshopUserDetails.java
+++ parent-module/backend/src/main/java/com/myshop/backend/admin/security/MyshopUserDetails.java
@@ -60,4 +60,7 @@ public class MyshopUserDetails implements UserDetails {
     public boolean isEnabled() {
         return user.isEnabled();
     }
+    public String getFullname() {
+        return this.user.getFirstName() + " " + this.user.getLastName();
+    }
 }
