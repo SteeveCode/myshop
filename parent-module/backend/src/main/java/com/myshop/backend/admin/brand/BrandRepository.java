4455725eee3ef6bb2894aa70a869d94b956c3b88
steevcode <stevecode@yahoo.com>
2023-04-06 12:26:46 +0100

    Section 11: Code Brand Management Module -  Exercise: Code Pagination, Sort and Filter for Brands Listing

--- parent-module/backend/src/main/java/com/myshop/backend/admin/brand/BrandRepository.java
+++ parent-module/backend/src/main/java/com/myshop/backend/admin/brand/BrandRepository.java
@@ -1,7 +1,10 @@
 package com.myshop.backend.admin.brand;
 
 import com.myshop.common.entity.Brand;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.PagingAndSortingRepository;
 
 
@@ -9,6 +12,9 @@ public interface BrandRepository extends JpaRepository<Brand, Integer> {
         public Long countById(Integer id);
 
         public Brand findByName(String name);
+
+        @Query("SELECT b FROM Brand b WHERE b.name LIKE %?1%")
+        public Page<Brand> findAll(String keyword, Pageable pageable);
 
 
 }
