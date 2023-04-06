642305eda3f4e5b59722a14dec89ee09a5d71081
steevcode <stevecode@yahoo.com>
2023-04-06 21:10:04 +0100

    Section 12: Code Product Management Module - Code Product Overview Form

--- parent-module/backend/src/main/java/com/myshop/backend/admin/brand/BrandRepository.java
+++ parent-module/backend/src/main/java/com/myshop/backend/admin/brand/BrandRepository.java
@@ -5,10 +5,12 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.PagingAndSortingRepository;
+
+import java.util.List;
 
 
 public interface BrandRepository extends JpaRepository<Brand, Integer> {
+
         public Long countById(Integer id);
 
         public Brand findByName(String name);
@@ -16,5 +18,6 @@ public interface BrandRepository extends JpaRepository<Brand, Integer> {
         @Query("SELECT b FROM Brand b WHERE b.name LIKE %?1%")
         public Page<Brand> findAll(String keyword, Pageable pageable);
 
-
+        @Query("SELECT NEW Brand(b.id, b.name) FROM Brand b ORDER BY b.name ASC")
+        public List<Brand> findAll();
 }
