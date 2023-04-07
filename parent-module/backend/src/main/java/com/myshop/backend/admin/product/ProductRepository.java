ddfb45ae7776494fa5f1fecab4d13b12f174dbd0
steevcode <stevecode@yahoo.com>
2023-04-07 11:18:54 +0100

    Section 12: Code Product Management Module - Exercise: Check Uniqueness of Product

--- parent-module/backend/src/main/java/com/myshop/backend/admin/product/ProductRepository.java
+++ parent-module/backend/src/main/java/com/myshop/backend/admin/product/ProductRepository.java
@@ -4,5 +4,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.myshop.common.entity.Product;
 
 public interface ProductRepository extends JpaRepository<Product, Integer> {
+    public Product findByName(String name);
 
 }
