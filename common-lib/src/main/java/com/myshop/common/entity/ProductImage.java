2f25bab6186c7a66dd0da4d8f4f7fd459600f38f
steevcode <stevecode@yahoo.com>
2023-04-09 09:00:37 +0100

    Section 12: Code Product Management Module - Code Edit Product Function

--- common-lib/src/main/java/com/myshop/common/entity/ProductImage.java
+++ common-lib/src/main/java/com/myshop/common/entity/ProductImage.java
@@ -1,13 +1,6 @@
 package com.myshop.common.entity;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 
 @Entity
 @Table(name = "product_images")
@@ -16,14 +9,14 @@ public class ProductImage {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
-	
+
 	@Column(nullable = false)
 	private String name;
-	
+
 	@ManyToOne
 	@JoinColumn(name = "product_id")
 	private Product product;
-	
+
 	public ProductImage() {
 	}
 
@@ -55,6 +48,10 @@ public class ProductImage {
 	public void setProduct(Product product) {
 		this.product = product;
 	}
-	
-	
+
+	@Transient
+	public String getImagePath() {
+		return "/product-images/" + product.getId() + "/extras/" + this.name;
+	}
+
 }
--- parent-module/backend/src/main/java/com/myshop/backend/admin/product/ProductService.java
+++ parent-module/backend/src/main/java/com/myshop/backend/admin/product/ProductService.java
@@ -2,6 +2,7 @@ package com.myshop.backend.admin.product;
 
 import java.util.Date;
 import java.util.List;
+import java.util.NoSuchElementException;
 
 import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -63,5 +64,13 @@ public class ProductService {
 		}
 
 		repo.deleteById(id);
+	}
+
+	public Product get(Integer id) throws ProductNotFoundException {
+		try {
+			return repo.findById(id).get();
+		} catch (NoSuchElementException ex) {
+			throw new ProductNotFoundException("Could not find any product with ID " + id);
+		}
 	}
 }
