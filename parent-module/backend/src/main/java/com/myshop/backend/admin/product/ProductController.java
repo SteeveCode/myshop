69a21b002223b2aac3e093ff480e05664d384c87
steevcode <stevecode@yahoo.com>
2023-04-10 13:44:13 +0100

    Section 12: Code Product Management Module - Code Filter Products by Category

--- parent-module/backend/src/main/java/com/myshop/backend/admin/product/ProductController.java
+++ parent-module/backend/src/main/java/com/myshop/backend/admin/product/ProductController.java
@@ -10,7 +10,9 @@ import java.util.Set;
 
 import com.myshop.backend.admin.FileUploadUtil;
 import com.myshop.backend.admin.brand.BrandService;
+import com.myshop.backend.admin.category.CategoryService;
 import com.myshop.common.entity.Brand;
+import com.myshop.common.entity.Category;
 import com.myshop.common.entity.ProductImage;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -29,26 +31,31 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+
 @Controller
 public class ProductController {
 	private static final Logger LOGGER = LoggerFactory.getLogger(ProductController.class);
 
 	@Autowired private ProductService productService;
 	@Autowired private BrandService brandService;
+	@Autowired private CategoryService categoryService;
 
 	@GetMapping("/products")
 	public String listFirstPage(Model model) {
-		return listByPage(1, model, "name", "asc", null);
+		return listByPage(1, model, "name", "asc", null, 0);
 	}
 
 	@GetMapping("/products/page/{pageNum}")
 	public String listByPage(
 			@PathVariable(name = "pageNum") int pageNum, Model model,
 			@Param("sortField") String sortField, @Param("sortDir") String sortDir,
-			@Param("keyword") String keyword
+			@Param("keyword") String keyword,
+			@Param("categoryId") Integer categoryId
 	) {
-		Page<Product> page = productService.listByPage(pageNum, sortField, sortDir, keyword);
+		Page<Product> page = productService.listByPage(pageNum, sortField, sortDir, keyword, categoryId);
 		List<Product> listProducts = page.getContent();
+
+		List<Category> listCategories = categoryService.listCategoriesUsedInForm();
 
 		long startCount = (pageNum - 1) * ProductService.PRODUCTS_PER_PAGE + 1;
 		long endCount = startCount + ProductService.PRODUCTS_PER_PAGE - 1;
@@ -57,6 +64,8 @@ public class ProductController {
 		}
 
 		String reverseSortDir = sortDir.equals("asc") ? "desc" : "asc";
+
+		if (categoryId != null) model.addAttribute("categoryId", categoryId);
 
 		model.addAttribute("currentPage", pageNum);
 		model.addAttribute("totalPages", page.getTotalPages());
@@ -68,6 +77,7 @@ public class ProductController {
 		model.addAttribute("reverseSortDir", reverseSortDir);
 		model.addAttribute("keyword", keyword);
 		model.addAttribute("listProducts", listProducts);
+		model.addAttribute("listCategories", listCategories);
 
 		return "products/products";
 	}
