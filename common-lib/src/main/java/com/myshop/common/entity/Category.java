e7b73f743aef398f2650cbe0a80c9fa1d840a3cd
steevcode <stevecode@yahoo.com>
2023-04-10 08:57:59 +0100

    Section 12: Code Product Management Module - Update Category Module for Product

--- common-lib/src/main/java/com/myshop/common/entity/Category.java
+++ common-lib/src/main/java/com/myshop/common/entity/Category.java
@@ -23,6 +23,9 @@ public class Category {
 
 	private boolean enabled;
 
+	@Column(name = "all_parent_ids", length = 256, nullable = true)
+	private String allParentIDs;
+
 	@OneToOne
 	@JoinColumn(name = "parent_id")
 	private Category parent;
@@ -168,5 +171,14 @@ public class Category {
 	public String toString() {
 		return this.name;
 	}
+
+	public String getAllParentIDs() {
+		return allParentIDs;
+	}
+
+	public void setAllParentIDs(String allParentIDs) {
+		this.allParentIDs = allParentIDs;
+	}
+
 
 }
