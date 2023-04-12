5dff7fe5a8125784445ff2b2ffdfda6bcb485415
steevcode <stevecode@yahoo.com>
2023-04-12 07:25:54 +0100

    Section 13: Code Listing Products for Customers - Code Listing Products in Category

---
+++ parent-module/frontend/src/main/java/com/myshop/product/ProductRepository.java
@@ -0,0 +1,16 @@
+package com.myshop.product;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import com.myshop.common.entity.Product;
+
+public interface ProductRepository extends JpaRepository<Product, Integer> {
+
+	@Query("SELECT p FROM Product p WHERE p.enabled = true "
+			+ "AND (p.category.id = ?1 OR p.category.allParentIDs LIKE %?2%)"
+			+ " ORDER BY p.name ASC")
+	public Page<Product> listByCategory(Integer categoryId, String categoryIDMatch, Pageable pageable);
+}
