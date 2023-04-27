b8684f8278f9392bec035268f0414fa8e5a06668
steevcode <stevecode@yahoo.com>
2023-04-27 11:46:47 +0100

    Section 18: Code Shopping Cart Module - Format Currency in Shopping Cart

--- parent-module/frontend/src/main/java/com/myshop/setting/SettingFilter.java
+++ parent-module/frontend/src/main/java/com/myshop/setting/SettingFilter.java
@@ -19,27 +19,28 @@ import com.myshop.common.entity.Setting;
 public class SettingFilter implements Filter {
 
 	@Autowired
-	private SettingService service; 
-	
+	private SettingService service;
+
 	@Override
 	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
 			throws IOException, ServletException {
-		
+
 		HttpServletRequest servletRequest = (HttpServletRequest) request;
 		String url = servletRequest.getRequestURL().toString();
-		
+
 		if (url.endsWith(".css") || url.endsWith(".js") || url.endsWith(".png") ||
 				url.endsWith(".jpg")) {
 			chain.doFilter(request, response);
 			return;
 		}
-		
+
 		List<Setting> generalSettings = service.getGeneralSettings();
 
 		generalSettings.forEach(setting -> {
 			request.setAttribute(setting.getKey(), setting.getValue());
+			System.out.println(setting.getKey() + " == > " + setting.getValue());
 		});
-		
+
 		chain.doFilter(request, response);
 
 	}
