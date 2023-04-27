088a2b5cd3d58fc01b9e9c2a17e48ff04d381b31
steevcode <stevecode@yahoo.com>
2023-04-27 11:09:56 +0100

    Section 18: Code Shopping Cart Module - Code Remove Product from Shopping Cart

--- parent-module/frontend/src/main/java/com/myshop/shoppingcart/ShoppingCartRestController.java
+++ parent-module/frontend/src/main/java/com/myshop/shoppingcart/ShoppingCartRestController.java
@@ -3,6 +3,7 @@ package com.myshop.shoppingcart;
 import jakarta.servlet.http.HttpServletRequest;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -54,6 +55,20 @@ public class ShoppingCartRestController {
 			return String.valueOf(subtotal);
 		} catch (CustomerNotFoundException ex) {
 			return "You must login to change quantity of product.";
+		}
+	}
+
+	@DeleteMapping("/cart/remove/{productId}")
+	public String removeProduct(@PathVariable("productId") Integer productId,
+								HttpServletRequest request) {
+		try {
+			Customer customer = getAuthenticatedCustomer(request);
+			cartService.removeProduct(productId, customer);
+
+			return "The product has been removed from your shopping cart.";
+
+		} catch (CustomerNotFoundException e) {
+			return "You must login to remove product.";
 		}
 	}
 }
--- parent-module/frontend/src/main/resources/static/js/shopping_cart.js
+++ parent-module/frontend/src/main/resources/static/js/shopping_cart.js
@@ -3,42 +3,47 @@ $(document).ready(function() {
 		evt.preventDefault();
 		decreaseQuantity($(this));
 	});
-	
+
 	$(".linkPlus").on("click", function(evt) {
 		evt.preventDefault();
 		increaseQuantity($(this));
-	});	
+	});
+
+	$(".linkRemove").on("click", function(evt) {
+		evt.preventDefault();
+		removeProduct($(this));
+	});
 });
 
 function decreaseQuantity(link) {
 	productId = link.attr("pid");
 	quantityInput = $("#quantity" + productId);
 	newQuantity = parseInt(quantityInput.val()) - 1;
-	
+
 	if (newQuantity > 0) {
 		quantityInput.val(newQuantity);
 		updateQuantity(productId, newQuantity);
 	} else {
 		showWarningModal('Minimum quantity is 1');
-	}	
+	}
 }
 
 function increaseQuantity(link) {
-		productId = link.attr("pid");
-		quantityInput = $("#quantity" + productId);
-		newQuantity = parseInt(quantityInput.val()) + 1;
-		
-		if (newQuantity <= 5) {
-			quantityInput.val(newQuantity);
-			updateQuantity(productId, newQuantity);
-		} else {
-			showWarningModal('Maximum quantity is 5');
-		}	
+	productId = link.attr("pid");
+	quantityInput = $("#quantity" + productId);
+	newQuantity = parseInt(quantityInput.val()) + 1;
+
+	if (newQuantity <= 5) {
+		quantityInput.val(newQuantity);
+		updateQuantity(productId, newQuantity);
+	} else {
+		showWarningModal('Maximum quantity is 5');
+	}
 }
 
 function updateQuantity(productId, quantity) {
 	url = contextPath + "cart/update/" + productId + "/" + quantity;
-	
+
 	$.ajax({
 		type: "POST",
 		url: url,
@@ -50,7 +55,7 @@ function updateQuantity(productId, quantity) {
 		updateTotal();
 	}).fail(function() {
 		showErrorModal("Error while updating product quantity.");
-	});	
+	});
 }
 
 function updateSubtotal(updatedSubtotal, productId) {
@@ -60,11 +65,56 @@ function updateSubtotal(updatedSubtotal, productId) {
 
 function updateTotal() {
 	total = 0.0;
-	
+	productCount = 0;
+
 	$(".subtotal").each(function(index, element) {
+		productCount++;
 		total += parseFloat(element.innerHTML.replaceAll(",", ""));
 	});
-	
-	formattedTotal = $.number(total, 2);
-	$("#total").text(formattedTotal);
+
+	if (productCount < 1) {
+		showEmptyShoppingCart();
+	} else {
+		formattedTotal = $.number(total, 2);
+		$("#total").text(formattedTotal);
+	}
+
+}
+
+function showEmptyShoppingCart() {
+	$("#sectionTotal").hide();
+	$("#sectionEmptyCartMessage").removeClass("d-none");
+}
+
+function removeProduct(link) {
+	url = link.attr("href");
+
+	$.ajax({
+		type: "DELETE",
+		url: url,
+		beforeSend: function(xhr) {
+			xhr.setRequestHeader(csrfHeaderName, csrfValue);
+		}
+	}).done(function(response) {
+		rowNumber = link.attr("rowNumber");
+		removeProductHTML(rowNumber);
+		updateTotal();
+		updateCountNumbers();
+
+		showModalDialog("Shopping Cart", response);
+
+	}).fail(function() {
+		showErrorModal("Error while removing product.");
+	});
+}
+
+function removeProductHTML(rowNumber) {
+	$("#row" + rowNumber).remove();
+	$("#blankLine" + rowNumber).remove();
+}
+
+function updateCountNumbers() {
+	$(".divCount").each(function(index, element) {
+		element.innerHTML = "" + (index + 1);
+	});
 }
