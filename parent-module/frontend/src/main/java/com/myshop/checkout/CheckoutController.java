ed106ce2530db40b7a805e6e1d9d6db19e383987
steevcode <stevecode@yahoo.com>
2023-05-03 12:05:52 +0100

    Section 22: Code Checkout Module - Code Checkout Page

---
+++ parent-module/frontend/src/main/java/com/myshop/checkout/CheckoutController.java
@@ -0,0 +1,63 @@
+package com.myshop.checkout;
+
+import java.util.List;
+
+import jakarta.servlet.http.HttpServletRequest;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import com.myshop.Utility;
+import com.myshop.address.AddressService;
+import com.myshop.common.entity.Address;
+import com.myshop.common.entity.CartItem;
+import com.myshop.common.entity.Customer;
+import com.myshop.common.entity.ShippingRate;
+import com.myshop.customer.CustomerService;
+import com.myshop.shipping.ShippingRateService;
+import com.myshop.shoppingcart.ShoppingCartService;
+
+@Controller
+public class CheckoutController {
+
+	@Autowired private CheckoutService checkoutService;
+	@Autowired private CustomerService customerService;
+	@Autowired private AddressService addressService;
+	@Autowired private ShippingRateService shipService;
+	@Autowired private ShoppingCartService cartService;
+	
+	@GetMapping("/checkout")
+	public String showCheckoutPage(Model model, HttpServletRequest request) {
+		Customer customer = getAuthenticatedCustomer(request);
+		
+		Address defaultAddress = addressService.getDefaultAddress(customer);
+		ShippingRate shippingRate = null;
+		
+		if (defaultAddress != null) {
+			model.addAttribute("shippingAddress", defaultAddress.toString());
+			shippingRate = shipService.getShippingRateForAddress(defaultAddress);
+		} else {
+			model.addAttribute("shippingAddress", customer.toString());
+			shippingRate = shipService.getShippingRateForCustomer(customer);
+		}
+		
+		if (shippingRate == null) {
+			return "redirect:/cart";
+		}
+		
+		List<CartItem> cartItems = cartService.listCartItems(customer);
+		CheckoutInfo checkoutInfo = checkoutService.prepareCheckout(cartItems, shippingRate);
+		
+		model.addAttribute("checkoutInfo", checkoutInfo);
+		model.addAttribute("cartItems", cartItems);
+		
+		return "checkout/checkout";
+	}
+	
+	private Customer getAuthenticatedCustomer(HttpServletRequest request) {
+		String email = Utility.getEmailOfAuthenticatedCustomer(request);				
+		return customerService.getCustomerByEmail(email);
+	}	
+}
