9bbc9cd8609620a17238cbe1ad0f1d5ae9ab5ede
steevcode <stevecode@yahoo.com>
2023-05-04 22:11:03 +0100

    Section 22: Code Checkout Module - Code Send Order Confirmation E-mail

--- parent-module/frontend/src/main/java/com/myshop/Utility.java
+++ parent-module/frontend/src/main/java/com/myshop/Utility.java
@@ -1,8 +1,11 @@
 package com.myshop;
 
+import java.text.DecimalFormat;
+import java.text.DecimalFormatSymbols;
 import java.util.Properties;
 
 import com.myshop.security.oauth.CustomerOAuth2User;
+import com.myshop.setting.CurrencySettingBag;
 import jakarta.servlet.http.HttpServletRequest;
 
 import org.springframework.mail.javamail.JavaMailSenderImpl;
@@ -15,29 +18,32 @@ import org.springframework.security.oauth2.client.authentication.OAuth2Authentic
 public class Utility {
 	public static String getSiteURL(HttpServletRequest request) {
 		String siteURL = request.getRequestURL().toString();
-		
+
 		return siteURL.replace(request.getServletPath(), "");
 	}
-	
+
 	public static JavaMailSenderImpl prepareMailSender(EmailSettingBag settings) {
 		JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
-		
+
 		mailSender.setHost(settings.getHost());
 		mailSender.setPort(settings.getPort());
 		mailSender.setUsername(settings.getUsername());
 		mailSender.setPassword(settings.getPassword());
-		
+
 		Properties mailProperties = new Properties();
 		mailProperties.setProperty("mail.smtp.auth", settings.getSmtpAuth());
 		mailProperties.setProperty("mail.smtp.starttls.enable", settings.getSmtpSecured());
-		//the line of code below (line 30)was not in the course. It was a solution to email error suggested by a student
+
+		// the line of code below (line 30)was not in the course. It was a solution to email error suggested by a student
 		// MAIL_PORT was also changed from 587 in the course to 465 suggested by student
+
 		mailProperties.setProperty("mail.smtp.ssl.enable", "true");
-		
+
 		mailSender.setJavaMailProperties(mailProperties);
-		
+
 		return mailSender;
 	}
+
 	public static String getEmailOfAuthenticatedCustomer(HttpServletRequest request) {
 		Object principal = request.getUserPrincipal();
 		if (principal == null) return null;
@@ -54,5 +60,34 @@ public class Utility {
 		}
 
 		return customerEmail;
+	}
+
+	public static String formatCurrency(float amount, CurrencySettingBag settings) {
+		String symbol = settings.getSymbol();
+		String symbolPosition = settings.getSymbolPosition();
+		String decimalPointType = settings.getDecimalPointType();
+		String thousandPointType = settings.getThousandPointType();
+		int decimalDigits = settings.getDecimalDigits();
+
+		String pattern = symbolPosition.equals("Before price") ? symbol : "";
+		pattern += "###,###";
+
+		if (decimalDigits > 0) {
+			pattern += ".";
+			for (int count = 1; count <= decimalDigits; count++) pattern += "#";
+		}
+
+		pattern += symbolPosition.equals("After price") ? symbol : "";
+
+		char thousandSeparator = thousandPointType.equals("POINT") ? '.' : ',';
+		char decimalSeparator = decimalPointType.equals("POINT") ? '.' : ',';
+
+		DecimalFormatSymbols decimalFormatSymbols = DecimalFormatSymbols.getInstance();
+		decimalFormatSymbols.setDecimalSeparator(decimalSeparator);
+		decimalFormatSymbols.setGroupingSeparator(thousandSeparator);
+
+		DecimalFormat formatter = new DecimalFormat(pattern, decimalFormatSymbols);
+
+		return formatter.format(amount);
 	}
 }
