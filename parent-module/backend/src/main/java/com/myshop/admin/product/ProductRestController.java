c827d1455d9dae94546feb81132b082e029f3b3f
steevcode <stevecode@yahoo.com>
2023-05-11 00:20:37 +0100

    Section 23: Code Order Management Module (Part 2) - Code Add Products to an Order (Part 2)

--- parent-module/backend/src/main/java/com/myshop/admin/product/ProductRestController.java
+++ parent-module/backend/src/main/java/com/myshop/admin/product/ProductRestController.java
@@ -1,18 +1,26 @@
 package com.myshop.admin.product;
 
+import com.myshop.common.entity.product.Product;
+import com.myshop.common.exception.ProductNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.repository.query.Param;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class ProductRestController {
 
 	@Autowired private ProductService service;
-	
+
 	@PostMapping("/products/check_unique")
-	public String checkUnique(@RequestParam("id") Integer id, @RequestParam("name") String name) {
+	public String checkUnique(Integer id, String name) {
 		return service.checkUnique(id, name);
-	}	
+	}
+
+	@GetMapping("/products/get/{id}")
+	public ProductDTO getProductInfo(@PathVariable("id") Integer id)
+			throws ProductNotFoundException {
+		Product product = service.get(id);
+		return new ProductDTO(product.getName(), product.getMainImagePath(),
+				product.getDiscountPrice(), product.getCost());
+	}
 }
