5b99831d99a637b26ad306854b63d889b65bbb09
steevcode <stevecode@yahoo.com>
2023-05-11 14:27:12 +0100

    Section 23: Code Order Management Module (Part 2) - Exercise: Code Manage Track Records of an Order

--- common-lib/src/main/java/com/myshop/common/entity/order/OrderTrack.java
+++ common-lib/src/main/java/com/myshop/common/entity/order/OrderTrack.java
@@ -1,14 +1,11 @@
 package com.myshop.common.entity.order;
 
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.Date;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.EnumType;
-import jakarta.persistence.Enumerated;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 
 import com.myshop.common.entity.IdBasedEntity;
 
@@ -18,13 +15,13 @@ public class OrderTrack extends IdBasedEntity {
 
 	@Column(length = 256)
 	private String notes;
-	
+
 	private Date updatedTime;
-	
+
 	@Enumerated(EnumType.STRING)
 	@Column(length = 45, nullable = false)
 	private OrderStatus status;
-	
+
 	@ManyToOne
 	@JoinColumn(name = "order_id")
 	private Order order;
@@ -60,6 +57,21 @@ public class OrderTrack extends IdBasedEntity {
 	public void setOrder(Order order) {
 		this.order = order;
 	}
-	
-	
+
+	@Transient
+	public String getUpdatedTimeOnForm() {
+		DateFormat dateFormatter = new SimpleDateFormat("yyyy-MM-dd'T'hh:mm:ss");
+		return dateFormatter.format(this.updatedTime);
+	}
+
+	public void setUpdatedTimeOnForm(String dateString) {
+		DateFormat dateFormatter = new SimpleDateFormat("yyyy-MM-dd'T'hh:mm:ss");
+
+		try {
+			this.updatedTime = dateFormatter.parse(dateString);
+		} catch (ParseException e) {
+			e.printStackTrace();
+		}
+	}
+
 }
