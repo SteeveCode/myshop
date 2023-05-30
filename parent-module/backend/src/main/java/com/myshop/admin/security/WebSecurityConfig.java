476c18ec8affdfb5b68a312fa4102bd279bfdb67
steevecode <stevecode@yahoo.com>
2023-05-30 14:04:33 +0100

    Section 28: Code Sales Report Module - Code Sales Report by Date - Part 1

--- parent-module/backend/src/main/java/com/myshop/admin/security/WebSecurityConfig.java
+++ parent-module/backend/src/main/java/com/myshop/admin/security/WebSecurityConfig.java
@@ -47,7 +47,7 @@ public class WebSecurityConfig {
 
                 .requestMatchers("/products/**").hasAnyAuthority("Admin", "Editor")
                 .requestMatchers("/orders", "/orders/", "/orders/page/**", "/orders/detail/**").hasAnyAuthority("Admin", "Salesperson", "Shipper")
-                .requestMatchers("/customers/**", "/orders/**", "/get_shipping_cost").hasAnyAuthority("Admin", "Salesperson")
+                .requestMatchers("/customers/**", "/orders/**", "/get_shipping_cost", "/reports/**").hasAnyAuthority("Admin", "Salesperson")
                 .requestMatchers("/orders_shipper/update/**").hasAuthority("Shipper")
                 .anyRequest().authenticated()
                 .and()
