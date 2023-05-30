ed1ef1ef1930f35859b1257dc3f15791064535a9
steevecode <stevecode@yahoo.com>
2023-05-30 17:07:28 +0100

    Section 28: Code Sales Report Module - Code Sales Report by Date - Part 3

--- parent-module/backend/src/test/java/com/myshop/admin/report/ReportRestControllerTests.java
+++ parent-module/backend/src/test/java/com/myshop/admin/report/ReportRestControllerTests.java
@@ -16,12 +16,20 @@ import org.springframework.test.web.servlet.MockMvc;
 public class ReportRestControllerTests {
 
 	@Autowired private MockMvc mockMvc;
-	
+
 	@Test
 	@WithMockUser(username = "user1", password = "pass1", authorities = {"Salesperson"})
 	public void testGetReportDataLast7Days() throws Exception {
 		String requestURL = "/reports/sales_by_date/last_7_days";
-		
+
+		mockMvc.perform(get(requestURL)).andExpect(status().isOk()).andDo(print());
+	}
+
+	@Test
+	@WithMockUser(username = "user1", password = "pass1", authorities = {"Salesperson"})
+	public void testGetReportDataLast6Months() throws Exception {
+		String requestURL = "/reports/sales_by_date/last_6_months";
+
 		mockMvc.perform(get(requestURL)).andExpect(status().isOk()).andDo(print());
 	}
 }
